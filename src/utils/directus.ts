6b184372d65f9797d209d5bb635c97e40fbfd98e
Max Schmidt <max.schmidt@outlook.de>
2023-02-10 12:24:38 +0100

    revert to hostname
    Signed-off-by: Max Schmidt <max.schmidt@outlook.de>

--- src/utils/directus.ts
+++ src/utils/directus.ts
@@ -1,7 +1,9 @@
 import { Directus } from "@directus/sdk";
 export const directus = () => {
   try {
-    return new Directus(import.meta.env.CMS_URL);
+    return new Directus(
+      import.meta.env.DEV ? "http://" : "https://" + import.meta.env.CMS_URL
+    );
   } catch (err) {
     console.log(err);
   }
