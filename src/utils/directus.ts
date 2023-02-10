289aa79f2c98ad03f56bd8623bd1ca579c2dc2f3
Max Schmidt <max.schmidt@outlook.de>
2023-02-10 12:27:51 +0100

    remove console.log()
    Signed-off-by: Max Schmidt <max.schmidt@outlook.de>

--- src/utils/directus.ts
+++ src/utils/directus.ts
@@ -1,7 +1,6 @@
 import { Directus } from "@directus/sdk";
 export const directus = () => {
   try {
-    console.log(import.meta.env.DEV, import.meta.env.PROD);
     return new Directus(
       (import.meta.env.DEV ? "http://" : "https://") + import.meta.env.CMS_URL
     );
