1324420803a950e97b1b6667642fc365d38b9027
Max Schmidt <max.schmidt@outlook.de>
2023-02-10 12:27:16 +0100

    fix name resolution
    Signed-off-by: Max Schmidt <max.schmidt@outlook.de>

--- src/utils/directus.ts
+++ src/utils/directus.ts
@@ -1,8 +1,9 @@
 import { Directus } from "@directus/sdk";
 export const directus = () => {
   try {
+    console.log(import.meta.env.DEV, import.meta.env.PROD);
     return new Directus(
-      import.meta.env.DEV ? "http://" : "https://" + import.meta.env.CMS_URL
+      (import.meta.env.DEV ? "http://" : "https://") + import.meta.env.CMS_URL
     );
   } catch (err) {
     console.log(err);
